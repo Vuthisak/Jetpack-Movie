8ec3a88eafc42c41db182b0938d331b95919085f
khyvuthisak <khyvuthisak@gmail.com>
2022-05-12 22:40:03 +0700

    Update argument to pass data in navgation

--- app/src/main/java/com/example/movieapp/navigation/MovieNavigator.kt
+++ app/src/main/java/com/example/movieapp/navigation/MovieNavigator.kt
@@ -1,9 +1,11 @@
 package com.example.movieapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.movieapp.screens.detail.DetailScreen
 import com.example.movieapp.screens.home.HomeScreen
 
@@ -16,8 +18,14 @@ fun MovieNavigation() {
             HomeScreen(navController = navController)
         }
 
-        composable(MovieScreens.DetailScreen.name) {
-            DetailScreen(navController = navController)
+        composable(
+            MovieScreens.DetailScreen.name + "/{movie}",
+            arguments = listOf(navArgument(name = "movie") { type = NavType.StringType })
+        ) { backStackEntry ->
+            DetailScreen(
+                navController = navController,
+                backStackEntry.arguments?.getString("movie")
+            )
         }
     }
 }
--- app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/home/HomeScreen.kt
@@ -20,8 +20,8 @@ fun HomeScreen(navController: NavController) {
     Scaffold(
         topBar = {
             TopAppBar(
-                backgroundColor = Color.Magenta,
-                elevation = 5.dp
+                backgroundColor = Color.Transparent,
+                elevation = 0.dp
             ) {
                 Text(text = "Movies")
             }
@@ -49,7 +49,7 @@ fun MainContent(
         LazyColumn {
             items(items = movieList) {
                 MovieRow(movie = it) { movie ->
-                    navController.navigate(route = MovieScreens.DetailScreen.name)
+                    navController.navigate(route = MovieScreens.DetailScreen.name + "/$movie")
                 }
             }
         }
